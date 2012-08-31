c4b2a9cc0ac08bff3290ea0adc603f3495c2a4c0
Sergey Zhumatiy <serg@parallel.ru>
2012-08-31 14:11:14 +0400

    -play

--- app/procedures/add_user.rb
+++ app/procedures/add_user.rb
@@ -1,2 +1,8 @@
 class AddUser < Procedure
+  def perform
+    @comment="User not added - I don't like it"
+    host=@task.resource.cluster_user.cluster.host
+    user=@task.resource.cluster_user.user
+    false
+  end
 end
