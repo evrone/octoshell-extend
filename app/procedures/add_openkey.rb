dcfb558d4d8640bad380efa7dcbeee1648920d2e
Jan Bernacki <releu@me.com>
2012-08-31 14:03:49 +0400

    add add_publickey attributes

--- app/procedures/add_openkey.rb
+++ app/procedures/add_openkey.rb
@@ -1,2 +1,57 @@
 class AddOpenkey < Procedure
+  attr_accessor :host, :project_login, :user_login, :public_key
+  
+  def host
+    cluster.host
+  end
+  
+  def project_login
+    project.username
+  end
+  
+  def user_login
+    account.username
+  end
+  
+  def public_key
+    credential.public_key
+  end
+  
+  def additional_attributes
+    Hash[request.request_properties.map { |p| [p.name.to_sym, p.value] }]
+  end
+  
+private
+  
+  def cluster
+    task.resource.cluster_user.cluster
+  end
+  
+  def request
+    cluster_user.request
+  end
+  
+  def project
+    request.project
+  end
+  
+  def user
+    credential.user
+  end
+  
+  def cluster_user
+    access.cluster_user
+  end
+  
+  def access
+    task.resource
+  end
+  
+  def account
+    Account.where(state: 'active', project_id: project.id, user_id: user.id).first
+  end
+  
+  def credential
+    task.resource.credential
+  end
 end
--- app/procedures/add_user.rb
+++ app/procedures/add_user.rb
@@ -1,2 +1,3 @@
 class AddUser < Procedure
+  
 end
