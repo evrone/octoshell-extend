cbe0f5172519f8af958bac396a4436a0947f4e81
Jan Bernacki <releu@me.com>
2012-08-31 14:39:49 +0400

    add add_user attributes

--- app/procedures/add_user.rb
+++ app/procedures/add_user.rb
@@ -1,3 +1,31 @@
 class AddUser < Procedure
+  def host
+    cluster.host
+  end
   
+  def project_login
+    project.username
+  end
+  
+  def additional_attributes
+    Hash[request.request_properties.map { |p| [p.name.to_sym, p.value] }]
+  end
+  
+private
+
+  def cluster
+    cluster_user.cluster
+  end
+  
+  def project
+    cluster_user.project
+  end
+  
+  def cluster_user
+    task.resource
+  end
+  
+  def request
+    cluster_user.request
+  end
 end
