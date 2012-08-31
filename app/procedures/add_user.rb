0b73e1e4bf6a9d90eff3fdf498558356620efaca
Sergey Zhumatiy <serg@parallel.ru>
2012-08-31 14:35:58 +0400

    - play

--- app/procedures/add_user.rb
+++ app/procedures/add_user.rb
@@ -1,8 +1,64 @@
 class AddUser < Procedure
+
   def perform
-    @comment="User not added - I don't like it"
-    host=@task.resource.cluster_user.cluster.host
-    user=@task.resource.cluster_user.user
+    @comment="Add user: #{user_login}, #{project_login} on #{host}"
+    #host=@task.resource.cluster_user.cluster.host
+    #user=@task.resource.cluster_user.user
     false
   end
+
+  def host
+    cluster.host
+  end
+  
+  def project_login
+    project.username
+  end
+  
+  def user_login
+    account.username
+  end
+  
+  def public_key
+    credential.public_key
+  end
+  
+  def additional_attributes
+    Hash[request.request_properties.map { |p| [p.name.to_sym, p.value] }]
+  end
+  
+private
+  
+  def cluster
+    task.resource.cluster_user.cluster
+  end
+  
+  def request
+    cluster_user.request
+  end
+  
+  def project
+    request.project
+  end
+  
+  def user
+    credential.user
+  end
+  
+  def cluster_user
+    access.cluster_user
+  end
+  
+  def access
+    task.resource
+  end
+  
+  def account
+    Account.where(state: 'active', project_id: project.id, user_id: user.id).first
+  end
+  
+  def credential
+    task.resource.credential
+  end
+
 end
