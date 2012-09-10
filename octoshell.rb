436cf00c0fbbff06d5106fecaa2fac80f000fe3c
Jan Bernacki <releu@me.com>
2012-09-10 11:48:49 +0400

    fix session key

--- octoshell.rb
+++ octoshell.rb
@@ -3,7 +3,7 @@ require File.expand_path('init')
 class Octoshell < Sinatra::Base
   set :views, settings.root + '/app/views'
   use Rack::Session::Cookie, 
-        key: "_msu_session",
+        key: "_mgu_session",
         secret: "3d3bc09c12b68b78f694a99d6d915c176e17497e81cee098312b1e483914df203b9d537606d5cea6df5f63eaa44fd36aad9ab1b0b6209da42f8e2821e580dbf0"
   
   get '/run/' do
