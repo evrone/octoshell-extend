eeeb7ae95a898d26e6c74a56b582254747d26775
Sergey Zhumatiy <serg@parallel.ru>
2012-09-12 13:34:54 +0400

    - fix

--- app/procedures/add_openkey.rb
+++ app/procedures/add_openkey.rb
@@ -4,11 +4,11 @@ require 'cocaine'
 class AddOpenkey < KeyProcedure
   def perform
     @comment="Add key: #{user_login} / #{project_login} on #{host} with #{public_key}.\n"
-    
+
     key=public_key.shellescape
 
-#    @comment += `ssh -i #{KEY_PATH}/key octo@#{host} sudo /usr/octo/add_openkey '#{user_login}' '#{key}'`
-    line=Cocaine::CommandLine.new('ssh', '-i', "#{KEY_PATH}/key", "octo@#{host}",
+#    @comment += `ssh -i #{SSH_KEY_PATH}/key octo@#{host} sudo /usr/octo/add_openkey '#{user_login}' '#{key}'`
+    line=Cocaine::CommandLine.new('ssh', '-i', "#{SSH_KEY_PATH}/key", "octo@#{host}",
                                   'sudo', '/usr/octo/add_openkey', user_login, key)
     begin
       @comment << line.run
