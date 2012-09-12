4d4c93f27bbe1543f2cd3580b07c937de7052abc
Jan Bernacki <releu@me.com>
2012-09-12 12:51:44 +0400

    debug

--- app/procedures/add_openkey.rb
+++ app/procedures/add_openkey.rb
@@ -3,11 +3,13 @@ require 'shellwords'
 class AddOpenkey < KeyProcedure
   def perform
     @comment="Add key: #{user_login} / #{project_login} on #{host} with #{public_key}.\n"
-
+    
     key=public_key.shellescape
-
+    
     @comment += `ssh -i #{SSH_KEY_PATH} octo@#{host} sudo /usr/octo/add_openkey '#{user_login}' '#{key}'`
-
+    
+    @comment << $?.inspect
+    @comment << $?.exitstatus.inspect
     $?.exitstatus == 0
   end
 end
