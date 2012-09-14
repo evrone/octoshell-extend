ea67281a266bca774deefea76ed54c7cc384fb28
Sergey Zhumatiy <serg@parallel.ru>
2012-09-14 16:37:30 +0400

    - modified openkey passing

--- app/procedures/del_openkey.rb
+++ app/procedures/del_openkey.rb
@@ -6,7 +6,30 @@ class DelOpenkey < KeyProcedure
 
     escaped_key=public_key.shellescape
 
-    @comment += `ssh -i #{SSH_KEY_PATH} octo@#{host} sudo /usr/octo/del_openkey #{project_login} '#{escaped_key}'`
+#    @comment += `ssh -i #{SSH_KEY_PATH} octo@#{host} sudo /usr/octo/del_openkey #{project_login} '#{escaped_key}'`
+
+    path="/tmp/octo-#{rand(MAX_RAND)}"
+    File.open(path,'w') do |f|
+      f << public_key
+    end
+
+    line=Cocaine::CommandLine.new('scp', "-i #{SSH_KEY_PATH} #{path} octo@#{host}:#{path}")
+    begin
+      @comment << line.run
+    rescue Cocaine::ExitStatusError => e
+      @comment << "ERROR: #{e.inspect} (#{e.message})"
+      File.unlink(path)
+      return false
+    end
+    File.unlink(path)
+
+    line=Cocaine::CommandLine.new('ssh', "-i #{SSH_KEY_PATH} octo@#{host} sudo /usr/octo/add_openkey  #{user_login} #{path}")
+    begin
+      @comment << line.run
+    rescue Cocaine::ExitStatusError => e
+      @comment << "ERROR: #{e.inspect} (#{e.message})"
+      return false
+    end
 
     $?.exitstatus == 0
   end
