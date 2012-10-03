c81bb36b8b21d17935c4f30bcd5021f492850648
Jan Bernacki <releu@me.com>
2012-10-03 14:57:15 +0400

    fix ordering and summary

--- app/scripts/cluster_statistic.rb
+++ app/scripts/cluster_statistic.rb
@@ -10,13 +10,13 @@ class ClusterStatistic
   def top_by_size
     @csv.sort_by do |row|
       row[:size]
-    end.last(10)
+    end.last(10).reverse
   end
   
   def top_by_files
     @csv.sort_by do |row|
       row[:files]
-    end.last(10)
+    end.last(10).reverse
   end
   
 private
