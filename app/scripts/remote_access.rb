28e99ff13c43001e7d15493e749bbe7e3fe6648c
Jan Bernacki <releu@me.com>
2012-10-09 17:41:39 +0400

    debug

--- app/scripts/remote_access.rb
+++ app/scripts/remote_access.rb
@@ -14,8 +14,11 @@ private
     path = "/tmp/octo-#{rand(1000000000)}"
     File.open(path, 'w+') { |f| f.write public_key }
     cmd = Cocaine::CommandLine.new('scp', "-i #{SSH_KEY_PATH} #{path} octo@t60.parallel.ru:#{path}")
+    $logger.info cmd.command
+    cmd.run
     exec = "/usr/octo/check_openkey #{username} #{path}"
     cmd = Cocaine::CommandLine.new('ssh', "-i #{SSH_KEY_PATH} octo@t60.parallel.ru #{exec}")
+    $logger.info cmd.command
     cmd.run
   rescue Cocaine::ExitStatusError
     false
