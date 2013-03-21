e1d8c38b1cb78cf218be1d054b1b89e908867147
Jan Bernacki <releu@me.com>
2013-03-21 20:12:13 +0100

    debug

--- app/procedures/block_user.rb
+++ app/procedures/block_user.rb
@@ -1,6 +1,7 @@
 class BlockUser < UserProcedure
   def perform
-    @comment = `ssh -t -i #{SSH_KEY_PATH} octo@#{host}  sudo /usr/octo/block_user #{user_login}`
+    @comment = "ssh -t -i #{SSH_KEY_PATH} octo@#{host}  sudo /usr/octo/block_user #{user_login}"
+    `ssh -t -i #{SSH_KEY_PATH} octo@#{host}  sudo /usr/octo/block_user #{user_login}`
     $?.exitstatus == 0
   end
 end
