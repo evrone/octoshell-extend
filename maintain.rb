3c8fd3739d967af8e6148cf718c1cc9d5f8b7fde
Jan Bernacki <releu@me.com>
2013-04-26 13:59:32 +0400

    fix completing requests

--- maintain.rb
+++ maintain.rb
@@ -4,9 +4,8 @@ loop do
   if request = Request.for_maintain
     m = Maintainer.new(request)
     m.maintain!
-    Request.where(
-      id: request.id,
-      maintain_requested_at: request.maintain_requested_at
+    Request.where(id: request.id).where(
+      "maintain_requested_at <= ?",  request.maintain_requested_at + 1
     ).update_all(maintain_requested_at: nil)
   end
   
