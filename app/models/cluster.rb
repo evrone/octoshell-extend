fddc94c4517862c825adcff61767ac9a2367bf98
Jan Bernacki <releu@me.com>
2013-04-29 20:19:44 +0400

    save

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,5 +1,14 @@
 class Cluster < ActiveRecord::Base
+  class Log < ActiveRecord::Base
+    belongs_to :cluster
+  end
+  
   has_many :tickets
   has_many :cluster_fields
   has_many :cluster_projects
+  has_many :logs
+  
+  def log(msg)
+    logs.create! "#{Time.now} #{msg}"
+  end
 end
