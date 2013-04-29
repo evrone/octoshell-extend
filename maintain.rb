672d520b36f926deafd3fe6ffedeb1ba22693e19
Jan Bernacki <releu@me.com>
2013-04-29 12:34:53 +0400

    new daemon

--- maintain.rb
+++ maintain.rb
@@ -1,13 +1,28 @@
 require File.expand_path('../init', __FILE__)
 
-loop do
-  if request = Request.for_maintain
-    m = Maintainer.new(request)
-    m.maintain!
-    Request.where(id: request.id).where(
-      "maintain_requested_at <= ?",  request.maintain_requested_at + 1
-    ).update_all(maintain_requested_at: nil)
+$failed = false
+threads = Cluster.all.map do |cluster|
+  Thread.new do
+    loop do
+      $failed && return
+      
+      begin
+        if request = Request.where(cluster_id: cluster.id).for_maintain
+          m = Maintainer.new(request)
+          m.maintain!
+          Request.where(id: request.id).where(
+            "maintain_requested_at <= ?",  request.maintain_requested_at + 1
+          ).update_all(maintain_requested_at: nil)
+        end
+        sleep 1
+      rescue Server::Fail
+        $failed = true
+        Thread.current.exit
+      end
+    end
   end
-  
-  sleep 5
 end
+
+threads.each &:join
+
+
